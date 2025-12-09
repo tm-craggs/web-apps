33450060f5e2a3d0d044659890ea335a5d8a2ca4
Julia.Radzhabova <Julia.Radzhabova@onlyoffice.com>
2025-12-09 22:43:44 +0300

    Fix Bug 79022

--- apps/documenteditor/main/app/view/Toolbar.js
+++ apps/documenteditor/main/app/view/Toolbar.js
@@ -3416,7 +3416,7 @@ define([
                     '<span class="color" style="background: <%= color %>;"></span>',
                     '<% }) %>',
                     '</span>',
-                    '<span class="text"><%= caption %></span>',
+                    '<span class="text"><%- caption %></span>',
                     '</a>'
                 ].join(''));
 
