fb12d0375b39617e7798c547d328bf05d71652b9
Polina <polina.hudyakova@onlyoffice.com>
2025-12-19 16:03:26 +0300

    add lock for btnSearch

--- apps/spreadsheeteditor/main/app/controller/Main.js
+++ apps/spreadsheeteditor/main/app/controller/Main.js
@@ -1365,7 +1365,7 @@ define([
                 }
                 if (options.header) {
                     if (options.header.search)
-                        this.headerView && this.headerView.lockHeaderBtns('search', disable);
+                        this.headerView && this.headerView.lockHeaderBtns('search', disable, Common.enumLock.lostConnect);
                     this.headerView && this.headerView.lockHeaderBtns('undo', options.viewMode, Common.enumLock.lostConnect);
                     this.headerView && this.headerView.lockHeaderBtns('redo', options.viewMode, Common.enumLock.lostConnect);
                 }
